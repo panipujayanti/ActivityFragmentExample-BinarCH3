99d6829342981fd868858d38f60f337974dd6309
panipujayanti <panipujayanti@gmail.com>
2023-09-15 18:43:00 +0700

    finish feature navigation intent

---
+++ app/src/main/java/com/napa/activityfragmentexample/model/Person.kt
@@ -0,0 +1,13 @@
+package com.napa.activityfragmentexample.model
+
+import android.net.UrlQuerySanitizer
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name : String,
+    val job : String,
+    val profileDesc : String,
+    val profilePictUrl: String
+) : Parcelable
--- app/src/main/java/com/napa/activityfragmentexample/presentation/fragmenttwo/FragmentTwo.kt
+++ app/src/main/java/com/napa/activityfragmentexample/presentation/fragmenttwo/FragmentTwo.kt
@@ -5,14 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import coil.load
 import com.napa.activityfragmentexample.R
+import com.napa.activityfragmentexample.databinding.FragmentOneBinding
+import com.napa.activityfragmentexample.databinding.FragmentTwoBinding
+import com.napa.activityfragmentexample.model.Person
 
 class FragmentTwo : Fragment() {
+
+    companion object{
+        const val ARGS_PERSON = "ARGS_PERSON"
+    }
+    private lateinit var binding: FragmentTwoBinding
+    private val person : Person? by lazy {
+        //bundle arguments
+        //arguments?.getParcelable(ARGS_PERSON)
+
+
+        FragmentTwoArgs.fromBundle(arguments as Bundle).person
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentTwoBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        showProfileData()
+    }
+
+    private fun showProfileData() {
+        if (person != null) {
+            binding.llProfile.isVisible = true
+            binding.ivProfileImg.load(person?.profilePictUrl)
+            binding.tvProfileDesc.text = person?.profileDesc
+            binding.tvName.text = person?.name
+            binding.tvJob.text = person?.job
+        }else{
+            binding.llProfile.isVisible = false
+            Toast.makeText(requireContext(), "Profile is Null", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setClickListener() {
+        binding.btnNavigate.setOnClickListener {
+            navigateToFragmentThree()
+        }
+    }
+
+    private fun navigateToFragmentThree() {
+        findNavController().navigate(R.id.action_fragmentTwo_to_fragmentThree)
     }
 }
